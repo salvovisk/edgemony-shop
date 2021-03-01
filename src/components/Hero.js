60d2287f8333ddc564c340aa9e68e36118c59656
Salvatore Viscuso <salvo.visk@gmail.com>
2021-03-01 21:48:30 +0100

    implemented desktop view

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,8 +6,10 @@ function Hero(props) {
   return (
     <section className="Hero">
       <img src={cover} alt="coverImage" />
-      <h1>{title}</h1>
-      <h2>{description}</h2>
+      <div>
+        <h1>{title}</h1>
+        <h2>{description}</h2>
+      </div>
     </section>
   );
 }
