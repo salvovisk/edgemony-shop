098d33f79658fe68316454e7ce48fb52b4d1417f
Salvatore Viscuso <salvo.visk@gmail.com>
2021-03-08 16:52:26 +0100

    render of cart in header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,27 @@
-import "./Header.css"
+import "./Header.css";
 
-function Header(props) {
+function Header({ logo, isInCart }) {
+  function totalOfCart() {
+    const total = isInCart.map((obj) => {
+      return obj.price;
+    });
+
+    // console.log(typeof total[0]);
+    if (total.length > 0) {
+      const reducer = (accumulator, currentValue) => accumulator + currentValue;
+      return total.reduce(reducer);
+    }
+  }
+
   return (
     <header className="Header">
-      <img src={props.logo} alt="headerLogo" className="logo" />
+      <img src={logo} alt="headerLogo" className="logo" />
+      <span style={{ color: "white" }}>
+        {isInCart.length > 0 && isInCart.length}
+        {isInCart.length > 0 && <h6>€ {totalOfCart()}</h6>}
+      </span>
     </header>
   );
 }
 
-export default Header
+export default Header;
