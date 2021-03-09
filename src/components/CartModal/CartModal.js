55c14bb6cd86589d35aec54a9874ecc4b1f266c9
Salvatore Viscuso <salvo.visk@gmail.com>
2021-03-09 19:24:29 +0100

    removed qty buttons

--- src/components/CartModal/CartModal.js
+++ src/components/CartModal/CartModal.js
@@ -1,14 +1,13 @@
 import "./CartModal.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
-import { faMinus } from "@fortawesome/free-solid-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faPlus } from "@fortawesome/free-solid-svg-icons";
+// import { faMinus } from "@fortawesome/free-solid-svg-icons";
 
 function CartModal({ isOpen, closeCartModal, totalPrice, cart, setCart }) {
   const removeFromCart = (item) => {
     const newCart = cart.filter((product) => product.id !== item.id);
     setCart(newCart);
   };
-
 
   return (
     <div className={`CartModal ${isOpen ? `isOpen` : ""}`}>
@@ -35,12 +34,12 @@ function CartModal({ isOpen, closeCartModal, totalPrice, cart, setCart }) {
                     />{" "}
                     <h4>{item.quantity}</h4>{" "}
                     <FontAwesomeIcon
-                      // onClick={() => addQty(item.id)}
+                      onClick={() => addQty(item)}
                       icon={faPlus}
                       className="faiconqty"
                     />{" "}
                   </span> */}
-                  <h4>Price: {item.price * item.quantity} €</h4>
+                  <h4>Price: {item.price /* * item.quantity */} €</h4>
                   <button
                     className="removeCartBtn"
                     onClick={() => removeFromCart(item)}
