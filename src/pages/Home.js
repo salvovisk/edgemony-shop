607b4390e98fb15c6471b1ac003c28f24e3d12b7
Salvatore Viscuso <salvo.visk@gmail.com>
2021-03-18 18:31:05 +0100

    removed loader and error and changed the logic for api calls

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,8 +3,7 @@ import { fetchProducts, fetchCatogories } from "./../services/api";
 
 import Hero from "./../components/Hero/Hero";
 import ProductsSection from "./../components/ProductsSection/ProductsSection";
-import Loader from "./../components/Loader/Loader";
-import ErrorProduct from "./../components/ErrorProduct/ErrorProduct";
+
 import MainSec from "./../containers/Main";
 
 import data from "./../utilities/data";
@@ -13,30 +12,25 @@ import { AppContainer } from "./../styles/styles";
 
 let cache;
 
-function Home() {
-  // Api data logic
-
+function Home({ setIsLoading, setApiError, apiError }) {
   const [products, setProducts] = useState(cache ? cache.products : []);
   const [categories, setCategories] = useState(cache ? cache.categories : []);
-  const [isLoading, setLoading] = useState(false);
-  const [isError, setError] = useState("");
-  const [retry, setRetry] = useState(false);
 
   useEffect(() => {
     if (cache !== undefined) {
       return;
     }
-    setLoading(true);
-    setError("");
+    setIsLoading(true);
+    setApiError("");
     Promise.all([fetchProducts(), fetchCatogories()])
       .then(([products, categories]) => {
         setProducts(products);
         setCategories(categories);
         cache = { products, categories };
       })
-      .catch((err) => setError(err.message))
-      .finally(() => setLoading(false));
-  }, [retry]);
+      .catch((err) => setApiError(err.message))
+      .finally(() => setIsLoading(false));
+  }, [apiError]);
 
   return (
     <AppContainer>
@@ -46,17 +40,7 @@ function Home() {
           title={data.title}
           description={data.description}
         />
-        {isLoading ? (
-          <Loader />
-        ) : isError ? (
-          <ErrorProduct
-            message={isError}
-            close={() => setError("")}
-            retry={() => setRetry(!retry)}
-          />
-        ) : (
-          <ProductsSection products={products} categories={categories} />
-        )}
+        <ProductsSection products={products} categories={categories} />
       </MainSec>
     </AppContainer>
   );
--- src/pages/Product.js
+++ src/pages/Product.js
@@ -11,27 +11,27 @@ import {
   ProductDescription,
   ProductPriceAndBtn,
 } from "./../styles/styles.js";
-import Loader from "../components/Loader/Loader";
-import ErrorProduct from "../components/ErrorProduct/ErrorProduct";
 
-function Product({ addToCart, removeFromCart, isInCart }) {
+function Product({
+  addToCart,
+  removeFromCart,
+  isInCart,
+  setIsLoading,
+  setApiError,
+}) {
   let { productId } = useParams();
 
   const [product, setProduct] = useState({});
-  const [isLoading, setIsLoading] = useState(false);
-  const [apiError, setApiError] = useState("");
-  const [retry, setRetry] = useState(false);
 
   useEffect(() => {
     setIsLoading(true);
-    setApiError("");
     fetchProduct(productId)
       .then((product) => {
         setProduct(product);
       })
       .catch((err) => setApiError(err.message))
-      .finally(() => setIsLoading(false));
-  }, [productId, retry]);
+      setIsLoading(false)
+  }, [productId]);
 
   const toggleCart = () => {
     if (isInCart(product)) {
@@ -41,37 +41,25 @@ function Product({ addToCart, removeFromCart, isInCart }) {
     }
   };
 
-  return (
-    <>
-      {isLoading ? (
-        <Loader />
-      ) : apiError ? (
-        <ErrorProduct
-          message={apiError}
-          close={() => setApiError("")}
-          retry={() => setRetry(!retry)}
-        />
-      ) : (
-        <ProductContent>
-          <ProductImg src={product.image} alt="productImg" />
-          <ProductTextContent>
-            <h3>{product.title}</h3>
-            <ProductDescription>{product.description}</ProductDescription>
-            <ProductPriceAndBtn>
-              <DefaultBlueBtn
-                type="button"
-                className="handleCartBtn"
-                onClick={toggleCart}
-              >
-                {isInCart(product) ? `Remove from Cart` : `Add to Cart`}
-              </DefaultBlueBtn>
-              {` ${formatPrice(product.price)}`}
-            </ProductPriceAndBtn>
-          </ProductTextContent>
-        </ProductContent>
-      )}
-    </>
-  );
+  return product ? (
+    <ProductContent>
+      <ProductImg src={product.image} alt="productImg" />
+      <ProductTextContent>
+        <h3>{product.title}</h3>
+        <ProductDescription>{product.description}</ProductDescription>
+        <ProductPriceAndBtn>
+          <DefaultBlueBtn
+            type="button"
+            className="handleCartBtn"
+            onClick={toggleCart}
+          >
+            {isInCart(product) ? `Remove from Cart` : `Add to Cart`}
+          </DefaultBlueBtn>
+          {` ${formatPrice(product.price)}`}
+        </ProductPriceAndBtn>
+      </ProductTextContent>
+    </ProductContent>
+  ) : null;
 }
 
 export default Product;
