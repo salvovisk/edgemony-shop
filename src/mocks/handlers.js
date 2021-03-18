77543493e5634350e5b7f50135a059c68e0fbc5d
Salvatore Viscuso <salvo.visk@gmail.com>
2021-03-18 09:42:30 +0100

    updated handler.js

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -2,6 +2,97 @@ import { rest } from "msw";
 const products = require("./data/products.json");
 const categories = require("./data/categories.json");
 
+const localStoragePrefix = "_edgemony-shop";
+const localStorageJSON = localStorage.getItem(localStoragePrefix);
+const savedData = localStorageJSON
+  ? JSON.parse(localStorageJSON)
+  : { orders: [], carts: [], cartId: 0, v: "0.1" };
+
+function save() {
+  localStorage.setItem(localStoragePrefix, JSON.stringify(savedData));
+}
+
+{
+  if (!localStorage.getItem("edgemony-cart-id")) {
+    const newCart = createCart();
+    localStorage.setItem("edgemony-cart-id", newCart.id);
+  }
+  const currentCartId = JSON.parse(localStorage.getItem("edgemony-cart-id"));
+  const cart = getCart(currentCartId);
+  localStorage.setItem(
+    "edgemony-cart",
+    JSON.stringify(getCartWithProducts(cart))
+  );
+}
+
+class HttpError extends Error {
+  constructor(message, status) {
+    super(message);
+    this.status = status;
+  }
+}
+
+function getCart(cartId) {
+  const cart = savedData.carts.find((cart) => cart.id === cartId);
+  if (!cart) {
+    throw new HttpError("Cart not found", 404);
+  }
+  return cart;
+}
+
+function getCartWithProducts(cart) {
+  return {
+    ...cart,
+    items: cart.items.map(({ id: productId, quantity }) => {
+      const { id, title, price, image } = getProduct(productId, 400);
+      return { id, title, price, image, quantity };
+    }),
+  };
+}
+
+function createCart() {
+  const id = ++savedData.cartId;
+  const cart = { id, items: [] };
+  savedData.carts.push(cart);
+  save();
+  return cart;
+}
+
+function addToCart(cartId, productId, quantity) {
+  if (!cartId || !productId || quantity <= 0) {
+    throw new HttpError("Invalid data", 400);
+  }
+  const cart = getCart(cartId);
+  const { id } = getProduct(productId, 400); // check if the product exists
+  const index = cart.items.findIndex((item) => item.id === id);
+  if (index !== -1) {
+    cart.items[index].quantity = quantity;
+  } else {
+    cart.items.push({ id, quantity });
+  }
+  save();
+  return getCartWithProducts(cart);
+}
+
+function removeFromCart(cartId, productId) {
+  const cart = getCart(cartId);
+  const index = cart.items.findIndex((product) => product.id === productId);
+  if (index === -1) {
+    throw new HttpError("Item not found", 404);
+  }
+  cart.items.splice(index, 1);
+  save();
+  return getCartWithProducts(cart);
+}
+
+function getProduct(id, status = 404) {
+  const product = products.find((product) => product.id === id);
+  if (!product) {
+    throw new HttpError("Product not found", status);
+  }
+  return product;
+}
+
 const baseURL = "https://fakestoreapi.com";
 
 function getError() {
@@ -23,7 +114,7 @@ function getError() {
 
 function randomError(resolverFn) {
   return function (req, res, ctx) {
-    if (Math.random() > 0.8) {
+    if (Math.random() > 0.9) {
       const { status, message } = getError();
       return res(ctx.status(status), ctx.json({ message }));
     }
@@ -34,13 +125,15 @@ function randomError(resolverFn) {
 export const handlers = [
   rest.get(
     `${baseURL}/products`,
-    randomError(function orderItems(req, res, ctx) {
+    randomError(function getProductsApi(req, res, ctx) {
       const q = req.url.searchParams.get("q");
       const limit = parseInt(req.url.searchParams.get("limit"));
       const offset = parseInt(req.url.searchParams.get("offset")) || 0;
-      let filteredProducts = products;
+      let filteredProducts = products.map((product) => {
+        return product;
+      });
       if (q) {
-        filteredProducts = products.filter(
+        filteredProducts = filteredProducts.filter(
           (product) => product.title.search(new RegExp(q, "i")) !== -1
         );
       }
@@ -52,13 +145,13 @@ export const handlers = [
   ),
   rest.get(
     `${baseURL}/products/categories`,
-    randomError(function orderItems(req, res, ctx) {
+    randomError(function getCategoriesApi(req, res, ctx) {
       return res(ctx.status(200), ctx.json(categories));
     })
   ),
   rest.get(
     `${baseURL}/products/categories/:category`,
-    randomError(function orderItems(req, res, ctx) {
+    randomError(function getCategoryApi(req, res, ctx) {
       const { category } = req.params;
       const filteredProducts = products.filter((p) => p.category === category);
       return res(ctx.status(200), ctx.json(filteredProducts));
@@ -66,13 +159,72 @@ export const handlers = [
   ),
   rest.get(
     `${baseURL}/products/:productId`,
-    randomError(function orderItems(req, res, ctx) {
-      const { productId } = req.params;
-      const product = products.find((p) => p.id + "" === productId);
-      if (product) {
-        return res(ctx.status(200), ctx.json(product));
+    randomError(function getProductApi(req, res, ctx) {
+      const productId = parseInt(req.params.productId);
+      return res(ctx.status(200), ctx.json(getProduct(productId)));
+    })
+  ),
+  rest.get(
+    `${baseURL}/carts/:id`,
+    randomError(function getCartApi(req, res, ctx) {
+      const id = parseInt(req.params.id);
+      try {
+        const cart = getCart(id);
+        return res(ctx.status(200), ctx.json(getCartWithProducts(cart)));
+      } catch (error) {
+        if (error instanceof HttpError) {
+          return res(
+            ctx.status(error.status),
+            ctx.json({ message: error.message })
+          );
+        }
+        return res(ctx.status(500), ctx.json({ message: error.message }));
       }
-      return res(ctx.status(404));
+    })
+  ),
+  rest.post(
+    `${baseURL}/carts/:id/items`,
+    randomError(function addToCartApi(req, res, ctx) {
+      try {
+        const id = parseInt(req.params.id);
+        const { id: productId, quantity } = JSON.parse(req.body);
+        return res(
+          ctx.status(200),
+          ctx.json(addToCart(id, productId, quantity))
+        );
+      } catch (error) {
+        if (error instanceof HttpError) {
+          return res(
+            ctx.status(error.status),
+            ctx.json({ message: error.message })
+          );
+        }
+        return res(ctx.status(500), ctx.json({ message: error.message }));
+      }
+    })
+  ),
+  rest.post(
+    `${baseURL}/carts`,
+    randomError(function createCartApi(req, res, ctx) {
+      return res(ctx.status(200), ctx.json(createCart()));
+    })
+  ),
+  rest.delete(
+    `${baseURL}/carts/:id/items/:productId`,
+    randomError(function removeCartItemApi(req, res, ctx) {
+      try {
+        const id = parseInt(req.params.id);
+        const productId = parseInt(req.params.id);
+        return res(ctx.status(200), ctx.json(removeFromCart(id, productId)));
+      } catch (error) {
+        if (error instanceof HttpError) {
+          return res(
+            ctx.status(error.status),
+            ctx.json({ message: error.message })
+          );
+        }
+        return res(ctx.status(500), ctx.json({ message: error.message }));
+      }
     })
   ),
 ];
